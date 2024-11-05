aca41aab6d1cdde3e983cdfc437d5a8ded463ef5
seonghun <kimsh8580@gmail.com>
2024-11-05 23:10:53 +0900

    week3: template 동적으로 생성하기

--- questions/views.py
+++ questions/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    name = 'kim'
+    context = {'name' : 'kim'}
+    return render(request, 'index.html', context)
