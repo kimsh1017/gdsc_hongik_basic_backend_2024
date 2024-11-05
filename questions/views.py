95a562c25e64ed81bad81b5acc63d5129a2c8906
seonghun <kimsh8580@gmail.com>
2024-11-05 23:15:39 +0900

    week4: 질문 목록, 상세 조회 함수  추가

--- questions/views.py
+++ questions/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from questions.models import Question
 
 # Create your views here.
-def index(request):
-    name = 'kim'
-    context = {'name' : 'kim'}
-    return render(request, 'index.html', context)
+def question_detail(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {'question' : question}
+    return render(request, 'question_detail.html', context)
+
+def question_list(request):
+    questions = Question.objects.all()
+    context = {'questions': questions}
+    return render(request, 'question_list.html', context)
