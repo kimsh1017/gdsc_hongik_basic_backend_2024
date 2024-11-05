36777a4b520525ba8177cae60320b39936922e32
seonghun <kimsh8580@gmail.com>
2024-11-05 23:19:53 +0900

    week5: 질문 생성 함수 추가

--- questions/views.py
+++ questions/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from questions.models import Question
+from django.utils import timezone
 
 # Create your views here.
 def question_detail(request, question_id):
@@ -11,3 +12,14 @@ def question_list(request):
     questions = Question.objects.all()
     context = {'questions': questions}
     return render(request, 'question_list.html', context)
+
+def question_create(request):
+    if request.method == 'GET':
+        return render(request, 'question_create.html')
+    elif request.method == 'POST':
+        Question.objects.create(
+            subject = request.POST["subject"],
+            content = request.POST["content"],
+            create_date = timezone.now()
+        )
+        return redirect('/questions')
